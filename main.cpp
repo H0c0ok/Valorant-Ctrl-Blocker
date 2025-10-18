c7cbca4da50db4191f6ddbcbe9094bbad038f5bd
H0c0ok <48594829+H0c0ok@users.noreply.github.com>
2025-10-18 23:50:00 +0300

    updated to release v0.2

--- main.cpp
+++ main.cpp
@@ -1,11 +1,53 @@
 ﻿#include <iostream>
 #include <windows.h>
+#include <tlhelp32.h>
+#include <tchar.h>
 #include <chrono>
 #include <thread>
 
 // Глобальная переменная: время, до которого блокируем Ctrl
 std::chrono::steady_clock::time_point blockUntil;
-uint32_t KeyBlockDuration = 500;
+uint32_t KeyBlockDuration = 0.5 * 1000;
+bool IsNeedToShowWarning = true;
+std::wstring ValorantProcessName = L"VALORANT";
+
+
+
+
+static bool IsValorantActive(void) {
+    bool IsValorantFound = false;
+    HANDLE hProcessSnap{ 0 };
+    PROCESSENTRY32 pe32{ 0 };
+    pe32.dwSize = sizeof(PROCESSENTRY32);
+    hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    std::wstring Temp = L"";
+    if (hProcessSnap == INVALID_HANDLE_VALUE) {
+        if (IsNeedToShowWarning) {
+            std::cerr << "[-] Error, unable to get list of running processes [-]" << std::endl;
+            std::cerr << " [?] This error leads to program being always active as long as it running [?]" << std::endl;
+            IsNeedToShowWarning = false;
+        }
+        return true;
+    }
+    if (!Process32First(hProcessSnap, &pe32)) {
+        if (IsNeedToShowWarning) {
+            std::cerr << "[-] Unable to get information about running processes [-]" << std::endl;
+            std::cerr << " [?] This error leads to program being always active as long as it running [?]" << std::endl;
+            IsNeedToShowWarning = false;
+        }
+    }
+
+    do {
+        Temp = pe32.szExeFile;
+        if (Temp.find(ValorantProcessName) != std::wstring::npos) {
+            IsValorantFound = true;
+            break;
+        }
+    } while (Process32Next(hProcessSnap, &pe32));
+    CloseHandle(hProcessSnap);
+    return IsValorantFound;
+}
+
 
 // Функция для проверки, нужно ли блокировать сейчас
 static bool shouldBlockCtrl() {
@@ -26,13 +68,14 @@ static LRESULT CALLBACK MouseHookProc(int nCode, WPARAM wParam, LPARAM lParam) {
 static LRESULT CALLBACK KeyboardHookProc(int nCode, WPARAM wParam, LPARAM lParam) {
     
     if (nCode >= 0) {
-        //KBDLLHOOKSTRUCT* kb = (KBDLLHOOKSTRUCT*)lParam;
         KBDLLHOOKSTRUCT* kb = reinterpret_cast<KBDLLHOOKSTRUCT*>(lParam);
         bool isCtrl = (kb->vkCode == VK_LCONTROL || kb->vkCode == VK_RCONTROL);
 
+        if (!IsValorantActive()) {
+            return CallNextHookEx(NULL, nCode, wParam, lParam);
+        }
 
         if (isCtrl && (wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN)) {
-            std::cout << "[?] Ctrl pressed [?]" << '\n';
             if (shouldBlockCtrl()) {
                 // Блокируем: не вызываем CallNextHookEx → событие не доходит до системы
                 std::cout << " [?] Skipping this ctrl DOWN [?]" << '\n';
@@ -40,7 +83,6 @@ static LRESULT CALLBACK KeyboardHookProc(int nCode, WPARAM wParam, LPARAM lParam
             }
         }
         if (isCtrl && (wParam == WM_KEYUP || wParam == WM_SYSKEYUP)) {
-            std::cout << "[?] Ctrl pressed [?]" << '\n';
             if (shouldBlockCtrl()) {
                 // Блокируем: не вызываем CallNextHookEx → событие не доходит до системы
                 std::cout << " [?] Skipping this ctrl UP [?]" << '\n';
@@ -50,6 +92,8 @@ static LRESULT CALLBACK KeyboardHookProc(int nCode, WPARAM wParam, LPARAM lParam
     }
     return CallNextHookEx(NULL, nCode, wParam, lParam);
 }
+
+
 
 
 int main() {
@@ -62,6 +106,7 @@ int main() {
 
     HHOOK hMouseHook = SetWindowsHookEx(WH_MOUSE_LL, MouseHookProc, GetModuleHandle(NULL), 0);
     HHOOK hKeyboardHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardHookProc, GetModuleHandle(NULL), 0);
+
 
     if (!hMouseHook || !hKeyboardHook) {
         MessageBox(NULL, L"Failed to install", L"Error", MB_ICONERROR);
